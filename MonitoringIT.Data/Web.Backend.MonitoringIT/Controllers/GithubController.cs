5df803be59abd1cafb50158cf7acea3ed610cc21
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-12-01 16:54:09 +0400

    add new actions getById and getByUsername

--- MonitoringIT.Data/Web.Backend.MonitoringIT/Controllers/GithubController.cs
+++ MonitoringIT.Data/Web.Backend.MonitoringIT/Controllers/GithubController.cs
@@ -4,16 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Database.MonitoringIT.DB.EfCore.Models;
 using DAL.MonitoringIT;
+using DAL.MonitoringIT.Implementation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Web.Backend.MonitoringIT.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class LinkedinController : ControllerBase
+    public class GithubController : ControllerBase
     {
-        // GET api/values
+        // GET: api/Github
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
@@ -21,8 +23,8 @@ namespace Web.Backend.MonitoringIT.Controllers
             {
                 try
                 {
-                    var linkedinProfiles = dal.LinkedinProfileDal.GetAll();
-                    return Ok(linkedinProfiles);
+                    var data = dal.GithubProfileDal.GetAll();
+                    return Ok(data);
                 }
                 catch (Exception e)
                 {
@@ -31,6 +33,7 @@ namespace Web.Backend.MonitoringIT.Controllers
             }
         }
 
+        // GET: api/Github/5
         [HttpGet("{id}")]
         public ActionResult GetById(int id)
         {
@@ -38,9 +41,9 @@ namespace Web.Backend.MonitoringIT.Controllers
             {
                 try
                 {
-                    var linkedinProfile = dal.LinkedinProfileDal.GetById(id);
-                    if (linkedinProfile is null) return NotFound();
-                    return Ok(linkedinProfile);
+                    var githubProfile = dal.GithubProfileDal.GetById(id);
+                    if (githubProfile is null) return NotFound();
+                    return Ok(githubProfile);
                 }
                 catch (Exception e)
                 {
@@ -48,16 +51,18 @@ namespace Web.Backend.MonitoringIT.Controllers
                 }
             }
         }
+
+        // GET: api/Github/5
         [HttpGet("user/{username}")]
-        public ActionResult GetByUsername(string username)
+        public ActionResult GetByUserName(string username)
         {
             using (var dal = new MonitoringDAL(""))
             {
                 try
                 {
-                    var linkedinProfile = dal.LinkedinProfileDal.GetByUserName(username);
-                    if (linkedinProfile is null) return NotFound();
-                    return Ok(linkedinProfile);
+                    var githubProfile = dal.GithubProfileDal.GetByUserName(username);
+                    if (githubProfile is null) return NotFound();
+                    return Ok(JsonConvert.SerializeObject(githubProfile,Formatting.Indented,new JsonSerializerSettings{ReferenceLoopHandling = ReferenceLoopHandling.Ignore}));
                 }
                 catch (Exception e)
                 {
@@ -65,5 +70,6 @@ namespace Web.Backend.MonitoringIT.Controllers
                 }
             }
         }
+
     }
 }
