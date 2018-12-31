ef52b0fbb84895e11aea2599387869e85fbc3788
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2018-12-31 18:59:50 +0400

    add new models

---
+++ MonitoringIT.Data/Web.Backend.MonitoringIT/Models/CrossProfileSearchModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Backend.MonitoringIT.Models
+{
+    public class CrossProfileSearchModel
+    {
+        public string Username { get; set; }
+        public ContentType ContentType { get; set; }
+    }
+
+    public enum ContentType
+    {
+        Github = 1,
+        Linkedin = 2
+    }
+}
---
+++ MonitoringIT.Data/Web.Backend.MonitoringIT/Models/ProfileCrossModel.cs
@@ -0,0 +1,10 @@
+﻿using Database.MonitoringIT.DB.EfCore.Models;
+
+namespace Web.Backend.MonitoringIT.Models
+{
+    public class ProfileCrossModel
+    {
+        public Profiles GithubProfile { get; set; }
+        public LinkedinProfile LinkedinProfile { get; set; }
+    }
+}
