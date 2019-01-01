4882e27ac49a18aa4c445e839c8201ca74009f59
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2019-01-01 22:06:10 +0400

    add git service

---
+++ MonitoringIT.Data/Services.MonitoringIT.Data.Github/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.MonitoringIT;
+using Lib.MonitoringIT.DATA.Github.Scrapper;
+
+namespace Services.MonitoringIT.Data.Github
+{
+    class Program
+    {
+        static void Main()
+        {
+            var githubScrapper = new GithubScrapper();
+            githubScrapper.Start();
+        }
+    }
+}
