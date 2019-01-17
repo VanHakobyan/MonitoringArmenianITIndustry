191b318dfd049d158f01458334670a4b296f3e31
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2019-01-18 00:21:22 +0400

    init github API integration

---
+++ MonitoringIT.Data/Services.MonitoringIT.Data.Integration.Github/GithubIntegrationService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Lib.MonitoringIT.Data.Github.Api;
+
+namespace Services.MonitoringIT.Data.Integration.Github
+{
+    public partial class GithubIntegrationService : ServiceBase
+    {
+        public GithubIntegrationService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            GithubApiProcessor apiProcessor=new GithubApiProcessor();
+            var githubUsers = apiProcessor.GetAllAsync().Result;
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        public void TestAndStart()
+        {
+            OnStart(null);
+            Console.ReadKey();
+            OnStop();
+        }
+    }
+}
