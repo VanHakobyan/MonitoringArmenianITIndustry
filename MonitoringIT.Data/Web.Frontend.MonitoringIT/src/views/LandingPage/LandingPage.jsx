8428c2251614adac17ed8477e0caac959cdaa85b
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2019-04-28 20:57:33 +0400

    minor fix in front

--- MonitoringIT.Data/Web.Frontend.MonitoringIT/src/views/LandingPage/LandingPage.jsx
+++ MonitoringIT.Data/Web.Frontend.MonitoringIT/src/views/LandingPage/LandingPage.jsx
@@ -58,10 +58,7 @@ class LandingPage extends React.Component {
               <GridItem xs={12} sm={12} md={6}>
                 <h1 className={classes.title}>Your Story Starts With Us.</h1>
                 <h4>
-                  Every landing page needs a small description after the big
-                  bold title, that's why we added this text here. Add here all
-                  the information that can make you or your product create the
-                  first impression.
+                    Monitoring Armenian IT industry and platform for automatic job hiring
                 </h4>
                 <br />
                 <Button
