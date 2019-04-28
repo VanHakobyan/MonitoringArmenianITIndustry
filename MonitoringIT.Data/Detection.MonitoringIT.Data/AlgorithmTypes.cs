44b5bede9e9b179d561cf4fb18703178c5403e9a
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2019-04-28 20:30:19 +0400

    Minor fix

--- MonitoringIT.Data/Detection.MonitoringIT.Data/AlgorithmTypes.cs
+++ MonitoringIT.Data/Detection.MonitoringIT.Data/AlgorithmTypes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Detection.MonitoringIT.Data
+﻿namespace Detection.MonitoringIT.Data
 {
     public static class AlgorithmTypes
     {
